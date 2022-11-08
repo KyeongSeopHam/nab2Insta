bcbc3160bfaba969135a78616b0aca8e0f355599
KyeongSeopHam <rudtjq5724@gmail.com>
2022-11-09 04:54:28 +0900

    시큐리티_로그인v1

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()    //해당 주소들만 인증이 필요하고  그게아닌 모든주소는  허용함
                 .and()                   //근데 다른주소쳤을시 걍 로그인페이지로가게해보자.and.formLogin.loginPage("/auth/signin")
                 .formLogin() // 인증이필요한 요청이오면 formLogin을할건데 그 폼로그인 페이지를 /auth/signin 임
-                .loginPage("/auth/signin")  // 열로 리다이렉션
+                .loginPage("/auth/signin")  // 열로 리다이렉션  GET  (어떤 인증이 필요한페이지가 필요하면 GET방식으로 호출을할거고 시큐리티컨피그가)
+                .loginProcessingUrl("auth/signin")  // POST (로그인) 누군가가 로그인요청을하면 POST
                 .defaultSuccessUrl("/"); // 로그인성공시 이페이지로가게하자
 
     }
--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -16,5 +16,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(CustomValidationException.class) //RuntimeException이 발생하는 모든 Exception을
     public String validationException(CustomValidationException e){ // 이 함수가(validationException) 가로챔
         return Script.back(e.getErorMap().toString());
+        /*CMRespDto와 Script만들어서 비교
+        1. 클라이언트에게 응답할땐 스크립이 좋음
+        -> 브라우저가받음
+        2. ajax통신 - CMRespDto  ->개발자가받음 코드로받으니
+        3. 안르도이드통신 CMRespDto ->개발자가받음 코드로받으니
+         */
     }
 }
