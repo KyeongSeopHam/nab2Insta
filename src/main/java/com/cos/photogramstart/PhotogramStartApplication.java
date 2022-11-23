c6d3389bac2e8b61350372f998d9dac2e8dfac82
KyeongSeopHam <rudtjq5724@gmail.com>
2022-11-24 06:26:17 +0900

    aaa

--- src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
@@ -2,16 +2,9 @@ package com.cos.photogramstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.cos.photogramstart")
-public class PhotogramStartApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return builder.sources(PhotogramStartApplication.class);
-	}
+@SpringBootApplication
+public class PhotogramStartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogramStartApplication.class, args);
 	}
